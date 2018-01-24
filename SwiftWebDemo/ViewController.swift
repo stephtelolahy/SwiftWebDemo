de9f029487d421e4596a14325a9192505155feee
Telolahy <stephano.telolahy@gmail.com>
2018-01-25 00:00:10 +0100

    update file

--- SwiftWebDemo/ViewController.swift
+++ SwiftWebDemo/ViewController.swift
@@ -22,7 +22,8 @@ class ViewController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string: "https://www.youtube.com")
+        let url = "https://firebasestorage.googleapis.com/v0/b/sweety-603e9.appspot.com/o/temp%2Farticle.html?alt=media&token=61926967-7198-4034-82d3-ea32c80272b4"
+        let myURL = URL(string: url)
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }
