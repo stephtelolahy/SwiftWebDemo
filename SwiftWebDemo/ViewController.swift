c8bd413b434fc8b4f4c5032412bb969d0137db16
Telolahy <stephano.telolahy@gmail.com>
2018-01-25 00:14:07 +0100

    Enable http

--- SwiftWebDemo/ViewController.swift
+++ SwiftWebDemo/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = "https://firebasestorage.googleapis.com/v0/b/sweety-603e9.appspot.com/o/temp%2Farticle.html?alt=media&token=61926967-7198-4034-82d3-ea32c80272b4"
+        let url = "https://firebasestorage.googleapis.com/v0/b/sweety-603e9.appspot.com/o/temp%2Farticle.html?alt=media&token=dd96d44b-1c90-4357-ac53-6574b5233e5e"
         let myURL = URL(string: url)
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
